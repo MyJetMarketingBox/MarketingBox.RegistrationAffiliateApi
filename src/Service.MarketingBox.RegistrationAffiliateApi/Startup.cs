a46dd6a74ebe3013280af56b1d35dc036cedefc9
Lev Kurbanov <lev.k@smplt.net>
2022-07-06 19:47:30 +0500

    foxoffers to cors

--- src/Service.MarketingBox.RegistrationAffiliateApi/Startup.cs
+++ src/Service.MarketingBox.RegistrationAffiliateApi/Startup.cs
@@ -47,7 +47,8 @@ namespace Service.MarketingBox.RegistrationAffiliateApi
                                 "http://localhost:3003",
                                 "http://marketing-box-frontend.marketing-box.svc.cluster.local:3000",
                                 "https://frontend-uat.trfme.biz",
-                                "https://frontend.trfme.biz")
+                                "https://frontend.trfme.biz",
+                                "https://foxoffers.com/")
                             .AllowCredentials()
                             .AllowAnyHeader()
                             .AllowAnyMethod();
